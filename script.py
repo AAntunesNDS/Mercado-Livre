118aa6538bdfba418de283573a928fb9a7bbd487
Arthur <arthur.antunes.aa@alu.ufc.br>
2020-02-05 15:38:08 -0300

    ordenado busca e selecionando o primeiro produto

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 
 class MercadoLivre:
@@ -6,6 +8,9 @@ class MercadoLivre:
         self.url =  'https://www.mercadolivre.com.br/'
         self.search_bar = 'as_word' # name
         self.btn_search = 'nav-search-btn' # class
+        self.btn_ord = 'ui-dropdown__indicator' # class
+        self.btn_menor_preco = 'Menor preço' # link text
+        self.elements_result = 'results-item highlighted article grid item-info-height-127' # class for xpath
 
     def navigate(self):
         self.driver.get(self.url)
@@ -16,9 +21,20 @@ class MercadoLivre:
         self.driver.find_elements_by_class_name(
             self.btn_search)[0].click()
 
+    def ordenar_busca(self):
+        self.driver.find_elements_by_class_name(
+            self.btn_ord)[0].click()
+        self.driver.find_element_by_link_text(
+            self.btn_menor_preco).click()
+
+    def click_element(self):
+        self.driver.find_elements_by_xpath(
+            "//li[@class='"+self.elements_result+"']")[0].click()
 
 
 ff = webdriver.Firefox()
 g = MercadoLivre(ff)
 g.navigate()
-g.search('Fone de ouvido')
+g.search('Sandalia feminina')
+g.ordenar_busca()
+g.click_element()
